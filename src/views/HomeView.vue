84359485dc37451a35248483bb604bb31e43e656
julianlau1998 <Julian.lau@aol.com>
2023-05-18 21:53:46 +0200

    removed ad

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,17 +1,15 @@
 <template>
   <div class="home">
     <Calculator />
-    <AdCard v-if="!iOS" class="mt-5" />
   </div>
 </template>
 
 <script>
 import Calculator from "@/components/Calculator/Calculator"
-import AdCard from "@/components/ads/AdCard"
 
 export default {
   name: 'HomeView',
-  components: { Calculator, AdCard },
+  components: { Calculator },
   computed: {
     iOS () {
       return this.$store.state.iOS
